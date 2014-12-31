3769c5cae8e2a8f7978d7cdb70ccc002fdcc7ced
Dominic Joseph <dominic@nectarom.com>
2014-12-31 17:45:38 -0600

    Try using middleware to send message.

--- app.js
+++ app.js
@@ -43,38 +43,36 @@ app.post('/', function (req, res, next) {
         return next();
     }
 
-    var options = {
-        url: 'https://api.groupme.com/v3/bots/post',
-        method: 'POST',
-        form: {}
-    };
-
-    switch (req.body.group_id) {
-        case '7771805': options.form.bot_id = '23d0b4561b9693e82424f9be63'; break;
-        case '11248555': options.form.bot_id = 'eeaab94daaef6eff88e1b3b68d'; break;
-        case '10059220': options.form.bot_id = '8db834f2d43673052c39a713a2'; break;
-        default: options.form.bot_id = 'eeaab94daaef6eff88e1b3b68d';
-    }
-
     if (req.body.text.match(/^alfred[.!?]?$/)) {
         shutup[req.body.group_id] = false;
         clearTimeout(shutupClock[req.body.group_id]);
-        options.form.text = 'Yes?';
+        req.reply = 'Yes?';
+        return next();
     }
 
-    else if (req.body.text.match(/lolol/)) options.form.text = 'Out loud out loud!';
+    else if (req.body.text.match(/lolol/)) {
+        req.reply = 'Out loud out loud!';
+        return next();
+    }
+
     else if (req.body.text.match(/alfred(,)? pug me/)) {
         request.get('http://pugme.herokuapp.com/random', function (err, r, b) {
             if (err) return next (err);
 
-            options.form.text = JSON.parse(b).pug;
-            setTimeout(function () {
-                request.post(options);
-            }, 1000);
+            req.reply = JSON.parse(b).pug;
+            return next();
         });
     }
-    else if (req.body.text.match(/thank(s| you)(,)? alfred[\.!\?]?/)) options.form.text = 'You are quite welcome, master.';
-    else if (req.body.text.match(/i('m| am) [A-z( )]*bored/)) options.form.text = 'Shut up, Dom.';
+
+    else if (req.body.text.match(/thank(s| you)(,)? alfred[\.!\?]?/)) {
+        req.reply = 'You are quite welcome, master.';
+        return next();
+    }
+
+    else if (req.body.text.match(/i('m| am) [A-z( )]*bored/)) {
+        req.reply = 'Shut up, Dom.';
+        return next();
+    }
 
     else if (req.body.text.match(/^alfred(,)? (compliment|insult) ([A-z'( )])+(.)?(!)?$/)) {
         var response, responseLength, split;
@@ -92,47 +90,70 @@ app.post('/', function (req, res, next) {
         var name = req.body.text.split(split)[1].replace('.', '').replace('!', '').trim();
 
         if (name === 'me') {
-            options.form.text = 'I\'m not programmed to lie.';
+            req.reply = 'I\'m not programmed to lie.';
         }
         else if (name === 'yourself' || name === 'I' || name === 'him' || name === 'her' || name === 'us' || name === 'them') {
-            options.form.text = 'I need names, master.';
+            req.reply = 'I need names, master.';
         }
         else {
             if (name.match(/\bmy\b/)) name = name.replace('my', 'your');
             name = name.charAt(0).toUpperCase() + name.substring(1);
 
-            options.form.text = name + response[Math.floor(Math.random() * responseLength)];
+            req.reply = name + response[Math.floor(Math.random() * responseLength)];
         }
+        return next();
     }
     else if (req.body.text.match(/\bfood\b/)) {
-        options.form.text = reply.food[Math.floor(Math.random() * foodTotal)];
+        req.reply = reply.food[Math.floor(Math.random() * foodTotal)];
+        return next();
     }
     else if (req.body.text.match(/^alfred(,)? say .*$/)) {
         var say = req.body.text.split('say ')[1].trim();
-        options.form.text = say.charAt(0).toUpperCase() + say.substring(1);
+        req.reply = say.charAt(0).toUpperCase() + say.substring(1);
+        return next();
     }
     else if (req.body.text.match(/\bmad\b/)) {
-        options.form.text = reply.mad[Math.floor(Math.random() * madTotal)];
+        req.reply = reply.mad[Math.floor(Math.random() * madTotal)];
+        return next();
     }
     else if (req.body.text.match(/\bwe should (do|go)\b/)) {
-        if (!(count++ % 5)) options.form.text = 'What a splendid idea! Count me in! Oh wait, I\'m not real.';
+        if (!(count++ % 5)) req.reply = 'What a splendid idea! Count me in! Oh wait, I\'m not real.';
+        return next();
     }
     else if (req.body.text.match(/\balfred(,)? shut( )?up\b/) || req.body.text.match(/\bshut( )?up(,)? alfred\b/)) {
-        options.form.text = reply.bye[Math.floor(Math.random() * byeTotal)];
+        req.reply = reply.bye[Math.floor(Math.random() * byeTotal)];
         shutup[req.body.group_id] = true;
         shutupClock[req.body.group_id] = setTimeout(function(){shutup[req.body.group_id] = false;}, 3600000);
+        return next();
     }
     else if (req.body.text.match(/^right(,)? alfred(\?)?/)) {
-        options.form.text = reply.right[Math.floor(Math.random() * rightTotal)];
+        req.reply = reply.right[Math.floor(Math.random() * rightTotal)];
+        return next();
     }
-    else return next();
+    else return res.send();
+});
 
-    res.send();
-    if (!req.body.text.match(/alfred(,)? pug me/)) {
-        setTimeout(function () {
-            request.post(options);
-        }, 1000);
+app.use(function (req, res, next) {
+    if (!req.reply) return;
+
+    var options = {
+        url: 'https://api.groupme.com/v3/bots/post',
+        method: 'POST',
+        form: {}
+    };
+
+    switch (req.body.group_id) {
+        case '7771805': options.form.bot_id = '23d0b4561b9693e82424f9be63'; break;
+        case '11248555': options.form.bot_id = 'eeaab94daaef6eff88e1b3b68d'; break;
+        case '10059220': options.form.bot_id = '8db834f2d43673052c39a713a2'; break;
+        default: options.form.bot_id = 'eeaab94daaef6eff88e1b3b68d';
     }
+
+    options.form.text = req.reply;
+
+    setTimeout(function () {
+        request.post(options);
+    }, 1000);
 });
 
 app.listen(process.env.PORT || 8666);
