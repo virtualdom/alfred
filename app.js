13fb7403e9c3bd669bf102714264e0ba6753b573
Dominic Joseph <dominic@nectarom.com>
2015-02-11 15:39:13 -0600

    Fix the Bible function.

--- app.js
+++ app.js
@@ -162,10 +162,14 @@ app.post('/', function (req, res, next) {
         var bible = req.body.text.split(req.body.text.match(/\bbible /i)[0])[1].trim();
 
         request.get('http://labs.bible.org/api/?passage=' + bible, function(e, r, b){
-            b = b.replace(/<b>[0-9:]+<\/b>/, '');
-            b.match(/&#[0-9]+;/g).forEach(function (el, i) {
-                b = b.replace(el, String.fromCharCode(el.replace('&#', '').replace(';', '')));
-            });
+            b = b.replace(/<b>[0-9:]+<\/b>/g, '');
+
+            if (b.match(/&#[0-9]+;/g)) {
+                b.match(/&#[0-9]+;/g).forEach(function (el, i) {
+                    b = b.replace(el, String.fromCharCode(el.replace('&#', '').replace(';', '')));
+                });
+            }
+
             req.reply = b;
 
             return next();
