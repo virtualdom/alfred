774e66c636adab3fa4480797123b5bf6aa083002
Dominic Joseph <dtpjoseph@gmail.com>
2016-07-17 23:57:25 -0700

    Update Giphy search to be more relevant.

--- app.js
+++ app.js
@@ -166,9 +166,9 @@ app.post('/', function (req, res, next) {
 
     else if (req.body.text.match(/^alfred(,)? giphy .*/i)) {
         var q = req.body.text.split('giphy ')[1].trim();
-        request('http://api.giphy.com/v1/gifs/random?api_key=' + credentials.giphy + '&tag=' + q, function(e, r, b) {
+        request('http://api.giphy.com/v1/gifs/search?limit=1&offset=' + _.random(0, 60) + '&api_key=' + credentials.giphy + '&q=' + q, function(e, r, b) {
             b = JSON.parse(b);
-            req.reply = b.data.image_original_url || 'Sorry, I could not find anything.';
+            req.reply = b.data[0].images.original.url || 'Sorry, I could not find anything.';
             return next();
         });
     }
